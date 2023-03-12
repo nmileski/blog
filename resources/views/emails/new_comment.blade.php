6ac54b98afd1ecd768db7ac2ee4042ece763e80a
Naum <naum.mileski@gmail.com>
2023-03-12 21:01:12 +0100

    initial

--- resources/views/emails/new_comment.blade.php
+++ resources/views/emails/new_comment.blade.php
@@ -1,9 +1,8 @@
 <x-mail::message>
 
-
-Comment: {{ $comment->content }}
-
-
+<p>Hello,</p>
+<p>There is a new comment on your post: {{ $post->title }}</p>
+<p>Comment: {{ $comment->content }}</p>
 
 Thanks,<br>
 {{ config('app.name') }}
